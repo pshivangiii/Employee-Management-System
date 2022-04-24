f0b7d231f82675d78692a78bf7eedb7b8941c5fe
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-24 13:12:11 +0530

    Add files via upload

--- app/AdminDetails.php
+++ app/AdminDetails.php
@@ -8,6 +8,11 @@ class AdminDetails extends Model
 {
     protected $table="admin_details";
     protected $primaryKey="id";
+    protected $fillable = [
+        'id',
+        'email',
+        'password'
+    ];
 
     /**
      * @param String $email
@@ -15,10 +20,13 @@ class AdminDetails extends Model
     */
     public static function adminRegistrationModel($email,$password)
     {
-        $reg=new AdminDetails();
-        $reg->email=$email;
-        $reg->password=$password;
-        $reg->save();
+        if((!empty($email)) && (!empty($password)))  
+        {
+            $reg=new AdminDetails();
+            $reg->email=$email;
+            $reg->password=$password;
+            $reg->save();
+        }
     }
     
     /**
@@ -28,6 +36,8 @@ class AdminDetails extends Model
     */
     public static function authenticateLogin($email,$password,$request)
     {
+     if((!empty($email)) && (!empty($password)))  
+     {
         $check=AdminDetails::where(['email'=>$email,'password'=>$password])->get();
         if(count($check)>0)
         {
@@ -36,20 +46,6 @@ class AdminDetails extends Model
             $users=compact('users');
             return $users;
         }
+     }
     }    
-
-    /**
-     * @param String $email
-     * @param $request
-    */
-    public static function enableLogin($email,$request)
-    {
-         $check=AdminDetails::where(['email'=>$email])->get();
-         if(count($check)>0)
-         {
-            $users=AdminDetails::where(['email'=>$email])->get();
-            $users=compact('users');
-            return $users;
-         }
-    }
 }
