1768ae4b9f33b3c3bcd6acea1028327b2fe01d09
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-26 08:49:21 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('attendance:user')->everyMinute();
+        $schedule->command('attendance:user')->monthly();
     }
 
     /**
