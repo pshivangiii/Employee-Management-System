1944ef7ba4b9be2292b01f9f7a200ca0fe86787b
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-26 10:23:24 +0530

    Added Filters

--- resources/views/filterview.blade.php
+++ resources/views/filterview.blade.php
@@ -1,36 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>USER DETAILS</title>
-</head>
-<body>
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Filter</title>
+  </head>
+  <body>
     <form class ="row g-3" action="{{url('/')}}/search" method="POST">
-        {{ csrf_field() }}
-    <h3>SEARCH</H3>
-        <div>
-        <label for="search"><b>Search</b></label>
-        <input type="text" placeholder="Enter Search" name="search" id="search" >
-        <button id = "bt" type="submit" >SEARCH</button>
+      {{ csrf_field() }}
+        <div class="container">
+            <h2 style="background-color:#a2bce7;">USERS:</h2><br>
+            <div>
+                <label for="search"><b>Search</b></label>
+                <input style="background-color:#ddd8c2;" type="text" placeholder="Enter Search" name="search" id="search" >
+                <button id = "bt" type="submit" >SEARCH</button>
+                </div><br>
+            <table class="table table-bordered table-striped">
+              <thead>
+               <tr style="background-color:#aa9cdf;">
+                  <th></th>
+                  <th>ID</th>
+                  <th>EMAIL</th> 
+                  <th>TEAM</th>
+                  <th>DESIGNATION</th>
+                </tr>
+              </thead>
+                @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->team}}</td>
+                    <td>{{ $user->designation}}</td>
+                @endforeach
+            </table>
+            {{ $users->appends($_GET)->links() }} </p>   
         </div>
-        <br>
-      <table border = "1">
-        <tr>
-            <td>ID</td>
-            <td>Email</td>
-            <td>Team</td>
-            <td>Designation</td>
-        </tr>
-        @foreach ($users as $user)
-        <tr>
-            <td>{{ $user->id }}</td>
-            <td>{{ $user->email }}</td>
-            <td>{{ $user->team}}</td>
-            <td>{{ $user->designation}}</td>
-        @endforeach
-      </table>
     </form>
-</body>
+  </body>
 </html>
