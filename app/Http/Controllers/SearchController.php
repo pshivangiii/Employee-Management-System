5f3913f0a09af2e6374be6e4b84a176fc86c454d
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-26 10:03:12 +0530

    Changes in SearchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,22 +8,46 @@ use App\EmployeeDetails;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 
-
-use Session;
-
-use Illuminate\Validation\Rule;
-
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
 
 class SearchController extends Controller
 {
-    //Custom search Filter
-    public function showFilter()
+     //Show users based with manual paginator
+     public function showUsers()
+     {
+       $id=5;
+       $data=EmployeeDetails::showUsers($id);
+       return view('showUsers',['data'=> $data]);
+     }
+     public function nextUsers($id)
+     {
+         $data=EmployeeDetails::nextUsers($id);
+         return view('showUsers',['data'=> $data]);
+     }
+     public function prevUsers($id)
+     {
+         $data=EmployeeDetails::prevUsers($id);
+         return view('showUsers',['data'=> $data]);
+     }
+
+    // Custom search Filter with pagination
+    public function showFilter(Request $request)
     {
-        $users=EmployeeDetails::allData();
-        return view('filterview',['users'=> $users]);
+        $users = EmployeeDetails::allData();
+        $filter_data = []; 
+        foreach($users as $row)
+        {
+            array_push($filter_data, $row);
+        }
+        $count = count($filter_data); 
+        $page = $request->page; 
+        $perPage = 3;
+        $offset = ($page-1) * $perPage;
+        $users = array_slice($filter_data, $offset, $perPage);
+        $users = new Paginator($users, $count, $perPage, $page, ['path'  => $request->url(),'query' => $request->query(),]);
+        return view('filterview',['users' => $users]);
     }
 
     public function showFilteredResult(Request $request)
@@ -31,24 +55,6 @@ class SearchController extends Controller
         $search= $request->input('search');
         $data=EmployeeDetails::searchData($search);
         return view('filter',['data'=> $data]);
-    }
-
-    //Show users based with manual paginator
-    public function showUsers()
-    {
-      $id=5;
-      $data=EmployeeDetails::showUsers($id);
-      return view('showUsers',['data'=> $data]);
-    }
-    public function nextUsers($id)
-    {
-        $data=EmployeeDetails::nextUsers($id);
-        return view('showUsers',['data'=> $data]);
-    }
-    public function prevUsers($id)
-    {
-        $data=EmployeeDetails::prevUsers($id);
-        return view('showUsers',['data'=> $data]);
     }
 }
 
