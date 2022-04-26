eacde4b3432fa7cbdc3e29218cf39779a2364086
pshivangiii <ShivangiPathak444@gmail.com>
2022-04-26 09:59:17 +0530

    Changes  in SearchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,10 +6,7 @@ use App\Info;
 use App\Adminreg;
 use App\EmployeeDetails;
 use Illuminate\Support\Facades\DB;
-
-use Session;
-
-use Illuminate\Validation\Rule;
+use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 
 use Illuminate\Http\Request;
 
@@ -17,73 +14,44 @@ use App\Http\Controllers\Controller;
 
 class SearchController extends Controller
 {
-    public function filter(){
-        $users = DB::select('select * from zzz');
-        return view('filter',['users'=>$users]);
-    }
-    public function postFilter(Request $request){
-        $search=$request->input('search');
-        $text=$request->input('text');
-        // return view('filterview',['users'=>$text]);
-        if($text == 'aaa'){
-            echo "We are here";
+     //Show users based with manual paginator
+     public function showUsers()
+     {
+       $id=5;
+       $data=EmployeeDetails::showUsers($id);
+       return view('showUsers',['data'=> $data]);
+     }
+     public function nextUsers($id)
+     {
+         $data=EmployeeDetails::nextUsers($id);
+         return view('showUsers',['data'=> $data]);
+     }
+     public function prevUsers($id)
+     {
+         $data=EmployeeDetails::prevUsers($id);
+         return view('showUsers',['data'=> $data]);
+     }
+
+    // Custom search Filter with pagination
+    public function showFilter(Request $request)
+    {
+        $users = EmployeeDetails::allData();
+        $filter_data = []; 
+        foreach($users as $row)
+        {
+            array_push($filter_data, $row);
         }
-        // if($search == 'Name'){
-        //     $users = DB::select('select id from zzz where email = ?', [$text]);
-        //     return view('filterview',['users'=>$users]);
-        // }
-        // else{
-        //     echo "NO RECORD FOUND";
-        // }
-
-        // if($search == 'Designation'){
-        //     $users = DB::select('select * from zzz where designation = ?', [$text]);
-        //     return view('filterview',['users'=>$users]);
-        // }
-        // else{
-        //     echo "NO RECORD FOUND";
-        // }
+        $count = count($filter_data); 
+        $page = $request->page; 
+        $perPage = 3;
+        $offset = ($page-1) * $perPage;
+        $users = array_slice($filter_data, $offset, $perPage);
+        $users = new Paginator($users, $count, $perPage, $page, ['path'  => $request->url(),'query' => $request->query(),]);
+        return view('filterview',['users' => $users]);
     }
-   
 
-
-
-    public function getSearch(){
-        return view('searchdemo');
-    }
-    public function postSearch(Request $request){
-
-    $search=$request['search']??"";
-
-    if($search!="")
-
+    public function showFilteredResult(Request $request)
     {
-
-      $val=EmployeeDetails::where('email','=',$search)->get();
-      
-
-    }
-
-    else
-
-    {
-
-      $val=EmployeeDetails::all();
-
-    }
-
-      $data=compact('val','data');
-
-    return view('searchdemo')->with($data);
-
-    }
-
-
-    public function filterview(){
-        $users=EmployeeDetails::AllData();
-        return view('filterview',['users'=> $users]);
-    }
-    public function postFilterview(Request $request){
         $search= $request->input('search');
         $data=EmployeeDetails::searchData($search);
         return view('filter',['data'=> $data]);
