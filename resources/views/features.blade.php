a0ab187da69b62bd1b803ae9e33920ee37f9c5f3
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-27 10:28:04 +0530

    Update features.blade.php

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -19,7 +19,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link" href="/adduser">Add User</a>
+          <a class="nav-link" href="/userPortal">Add User</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/show">Delete User</a>
