69927db67c867ba547682838441f280b69e84c05
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-27 11:53:33 +0530

    updated SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -15,46 +15,96 @@ use App\Http\Controllers\Controller;
 class SearchController extends Controller
 {
      //Show users based with manual paginator
-     public function showUsers()
-     {
-       $id=5;
-       $data=EmployeeDetails::showUsers($id);
-       return view('showUsers',['data'=> $data]);
-     }
-     public function nextUsers($id)
-     {
-         $data=EmployeeDetails::nextUsers($id);
-         return view('showUsers',['data'=> $data]);
-     }
-     public function prevUsers($id)
-     {
-         $data=EmployeeDetails::prevUsers($id);
-         return view('showUsers',['data'=> $data]);
-     }
+    public function showUsers()
+    {
+       try
+       {  
+           $id=5;
+           $data=EmployeeDetails::showUsers($id);
+           return view('showUsers',['data'=> $data]);
+       }
+       catch (\Exception $e) 
+       {
+           return redirect('error')->with
+           (
+              'error', $e->getMessage()
+           );
+       }
+    }
+    public function nextUsers($id)
+    {
+        try
+        {
+            $data=EmployeeDetails::nextUsers($id);
+            return view('showUsers',['data'=> $data]);
+        }
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
+        }
+    }
+    public function prevUsers($id)
+    {
+        try
+        {
+            $data=EmployeeDetails::prevUsers($id);
+            return view('showUsers',['data'=> $data]);
+        }
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
+        }
+    }
 
     // Custom search Filter with pagination
     public function showFilter(Request $request)
     {
-        $users = EmployeeDetails::allData();
-        $filter_data = []; 
-        foreach($users as $row)
+        try
         {
-            array_push($filter_data, $row);
+            $users = EmployeeDetails::allData();
+            $filter_data = []; 
+            foreach($users as $row)
+            {
+                array_push($filter_data, $row);
+            }
+            $count = count($filter_data); 
+            $page = $request->page; 
+            $perPage = 3;
+            $offset = ($page-1) * $perPage;
+            $users = array_slice($filter_data, $offset, $perPage);
+            $users = new Paginator($users, $count, $perPage, $page, ['path'  => $request->url(),'query' => $request->query(),]);
+            return view('filterview',['users' => $users]);
         }
-        $count = count($filter_data); 
-        $page = $request->page; 
-        $perPage = 3;
-        $offset = ($page-1) * $perPage;
-        $users = array_slice($filter_data, $offset, $perPage);
-        $users = new Paginator($users, $count, $perPage, $page, ['path'  => $request->url(),'query' => $request->query(),]);
-        return view('filterview',['users' => $users]);
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+                'error', $e->getMessage()
+            );
+        }
     }
 
     public function showFilteredResult(Request $request)
     {
-        $search= $request->input('search');
-        $data=EmployeeDetails::searchData($search);
-        return view('filter',['data'=> $data]);
+        try
+        {
+            $search= $request->input('search');
+            $data=EmployeeDetails::searchData($search);
+            return view('filter',['data'=> $data]);
+        }
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
+        }
     }
 }
 
